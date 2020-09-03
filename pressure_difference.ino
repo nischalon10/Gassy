77fcacefe2f09c6f26f7338c3325f5169d5e3487
nischalon10 <44796329+nischalon10@users.noreply.github.com>
2020-09-03 12:12:30 +0530

    Add files via upload

---
+++ pressure_difference.ino
@@ -0,0 +1,22 @@
+int vc = 2000;
+int po = 101325;
+float stk = 5.2;
+float t;
+double pd,piv,pivp,pivn,pc,vo;
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Pressure sensor r");
+}
+
+void loop() {
+  pivp = analogRead(A0);
+  pivn = analogRead(A1);
+  piv = pivp - pivn;
+  pd = ((piv/2046)*1000) - 2;
+  Serial.print(pd);
+  Serial.println();
+  delay(200);
+ 
+}
